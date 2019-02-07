6d09170f7a72349e4c9ba6ad2611a9b6a39b060c
Loise Mwarangu <loisemwarangu@gmail.com>
2019-02-07 14:44:35 +0300

    interpolated image to url

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,27 @@
 from flask import Flask
-from .config import config_options
 from flask_bootstrap import Bootstrap
+from config import config_options
 
 bootstrap = Bootstrap()
 
 def create_app(config_name):
+    """
+    Function that will initialize the Flask instance (app)
+    """
+    #create the app instance
     app = Flask(__name__)
+
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
     bootstrap.init_app(app)
+    
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # setting config
     from .request import configure_request
     configure_request(app)
 
