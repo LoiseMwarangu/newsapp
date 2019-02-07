f4202258fbd3a17bcc13555d0890a175fad19772
Loise Mwarangu <loisemwarangu@gmail.com>
2019-02-07 12:42:41 +0300

    fixed indentation errors

--- tests/news_test.py
+++ tests/news_test.py
@@ -1,14 +1,23 @@
 import unittest
-from .models import news
-News = news.News
 
-class NewsTest(unittest.TestCase):
+from app.models import Articles
+
+
+
+
+class articlesTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the articles class
+    '''
+
     def setUp(self):
-        self.new_news = News('Thesun.co.uk','Taxi ploughs into World Cup fans in Moscow leaving seven injured','Moscow police believe the driver may have lost control of the vehicle this afternoon','https://i2-prod.mirror.co.uk/incoming/article12730482.ece/ALTERNATES/s1200/Group-E-Brazil-vs-Switzerland-Rostov-On-Don-Russian-Federation-17-Jun-2018.jpg',2018-06-16)
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_articles = Articles(1234,'bbc news','John','new world','url','urlToImage','A thrilling new world','publishedAt')
 
-        def test_instance(self):
-            self.assertTrue(isinstance(self.new_news,News))
-
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_articles,Articles))
 
 if __name__ == '__main__':
     unittest.main()
